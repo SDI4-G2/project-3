cd443aa232b701842e39be4ffb6136ef17e27108
Ewen <ewenlke@hotmail.com>
2022-06-15 02:18:28 +0800

    Initial commit test data retrieval

---
+++ JCGPTUITION/containers/VideoScreen.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { Text, TouchableOpacity, StyleSheet, View, TextInput, Button } from 'react-native';
+
+import GetVideo from '../api/GetVideo';
+
+export default function VideoScreen() {
+  const [list, setList] = useState(undefined);
+
+  async function get() {
+    const videos = await GetVideo();
+    setList(videos);
+  }
+
+  return (
+    <View>
+      <Button title="Press" onPress={get}></Button>
+      <Text>{list}</Text>
+    </View>
+  );
+}
