3ba312e130abfe0de7d14bff8c295a9498ec1ef5
Ewen <ewenlke@hotmail.com>
2022-06-16 18:14:31 +0800

    update wheel load

--- project-3-app/containers/Dashboard.js
+++ project-3-app/containers/Dashboard.js
@@ -1,32 +1,24 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  ImageBackground,
-  RefreshControl,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import { Card, Title } from "react-native-paper";
-import React, { useState, useEffect } from "react";
-import * as SecureStore from "expo-secure-store";
-import HeaderDashboard from "../components/HeaderDashboard";
+import { StyleSheet, Text, View, ScrollView, ImageBackground, RefreshControl, ActivityIndicator, TouchableOpacity, Image } from 'react-native';
+import { Card, Title } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import HeaderDashboard from '../components/HeaderDashboard';
 
 export default function Dashboard({ route, navigation }) {
   const { username, email } = route.params;
   const [videos, setVideos] = useState([]);
   const [articles, setArticles] = useState([]);
   const [refreshing, setRefreshing] = React.useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetch_all_videos() {
-    let result = await SecureStore.getItemAsync("token");
+    let result = await SecureStore.getItemAsync('token');
 
-    const response = await fetch("https://sdi4-g2.herokuapp.com/video", {
-      method: "GET",
+    const response = await fetch('https://sdi4-g2.herokuapp.com/video', {
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + result,
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + result,
       },
     });
     const list = await response.json();
@@ -35,13 +27,13 @@ export default function Dashboard({ route, navigation }) {
   }
 
   async function fetch_all_articles() {
-    let result = await SecureStore.getItemAsync("token");
+    let result = await SecureStore.getItemAsync('token');
 
-    const response = await fetch("https://sdi4-g2.herokuapp.com/article", {
-      method: "GET",
+    const response = await fetch('https://sdi4-g2.herokuapp.com/article', {
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + result,
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + result,
       },
     });
     const list = await response.json();
@@ -66,12 +58,8 @@ export default function Dashboard({ route, navigation }) {
   }, []);
 
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-    >
-      <HeaderDashboard user_name={username} email={email}/>
+    <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+      <HeaderDashboard user_name={username} email={email} />
       <View style={styles.container}>
         <Text style={styles.mediumText}>Let's get started</Text>
 
@@ -80,17 +68,18 @@ export default function Dashboard({ route, navigation }) {
           <ScrollView
             horizontal={true}
             contentContainerStyle={{
-              justifyContent: "center",
-              flexDirection: "row",
+              justifyContent: 'center',
+              flexDirection: 'row',
             }}
           >
+            {isLoading === true && <ActivityIndicator color={'#fff'} />}
             {videos.map((item) => {
               return (
                 <Card
                   style={styles.cardDashboard}
                   key={item.videoid}
                   onPress={() => {
-                    navigation.navigate("VideoScreen", {
+                    navigation.navigate('VideoScreen', {
                       videoid: item.videoid,
                     });
                   }}
@@ -101,13 +90,12 @@ export default function Dashboard({ route, navigation }) {
                     imageStyle={{
                       borderRadius: 15,
                       opacity: 0.5,
-                      backgroundColor: "#000",
+                      backgroundColor: '#000',
                     }}
+                    onLoadEnd={() => setIsLoading(false)}
                   >
                     <Card.Content>
-                      <Title style={styles.cardTitle}>
-                        {item.Category.description}
-                      </Title>
+                      <Title style={styles.cardTitle}>{item.Category.description}</Title>
                     </Card.Content>
                   </ImageBackground>
                 </Card>
@@ -121,10 +109,11 @@ export default function Dashboard({ route, navigation }) {
           <ScrollView
             horizontal={true}
             contentContainerStyle={{
-              justifyContent: "center",
-              flexDirection: "row",
+              justifyContent: 'center',
+              flexDirection: 'row',
             }}
           >
+            {isLoading === true && <ActivityIndicator color={'#fff'} />}
             {articles.map((item, index) => (
               <Card style={styles.cardDashboard} key={item.articleid}>
                 <ImageBackground
@@ -134,13 +123,12 @@ export default function Dashboard({ route, navigation }) {
                   imageStyle={{
                     borderRadius: 15,
                     opacity: 0.5,
-                    backgroundColor: "#000",
+                    backgroundColor: '#000',
                   }}
+                  onLoadEnd={() => setIsLoading(false)}
                 >
                   <Card.Content>
-                    <Title style={styles.cardTitle}>
-                      {item.Category.description}
-                    </Title>
+                    <Title style={styles.cardTitle}>{item.Category.description}</Title>
                   </Card.Content>
                 </ImageBackground>
               </Card>
@@ -159,13 +147,13 @@ const styles = StyleSheet.create({
     padding: 15,
   },
   mediumText: {
-    fontStyle: "normal",
-    fontWeight: "400",
+    fontStyle: 'normal',
+    fontWeight: '400',
     fontSize: 14,
     lineHeight: 30,
-    display: "flex",
-    alignItems: "center",
-    color: "rgba(255, 255, 255, 0.7)",
+    display: 'flex',
+    alignItems: 'center',
+    color: 'rgba(255, 255, 255, 0.7)',
   },
   cardDashboard: {
     width: 200,
@@ -173,26 +161,26 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     marginRight: 15,
-    borderColor: "rgba(102, 112, 128, 0.3)",
+    borderColor: 'rgba(102, 112, 128, 0.3)',
     borderRadius: 15,
-    backgroundColor: "#d2d5db",
+    backgroundColor: '#d2d5db',
     borderWidth: 1,
     fontWeight: 700,
   },
   cardTitle: {
-    fontStyle: "normal",
-    fontWeight: "400",
+    fontStyle: 'normal',
+    fontWeight: '400',
     fontSize: 20,
     lineHeight: 30,
-    display: "flex",
-    alignItems: "center",
-    color: "rgba(255, 255, 255, 0.7)",
+    display: 'flex',
+    alignItems: 'center',
+    color: 'rgba(255, 255, 255, 0.7)',
   },
   cardImage: {
     width: 200,
     height: 200,
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
   },
 });
--- project-3-app/containers/SignUpScreen.js
+++ project-3-app/containers/SignUpScreen.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { Text, TouchableOpacity, StyleSheet, View, Button } from "react-native";
-import { TextInput } from "react-native-paper";
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, StyleSheet, View, Button, ActivityIndicator } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
-import Buttons from "../components/Buttons";
-import Register from "../api/Register";
-import SecondHeaderBar from "../components/SecondHeader";
-import Bold from "../assets/Poppins_Bold";
-import Small from "../assets/Poppins_Small";
-import Underline from "../assets/Poppins_Underline";
+import Buttons from '../components/Buttons';
+import Register from '../api/Register';
+import SecondHeaderBar from '../components/SecondHeader';
+import Bold from '../assets/Poppins_Bold';
+import Small from '../assets/Poppins_Small';
+import Underline from '../assets/Poppins_Underline';
 
 export default function SignUpScreen({ navigation }) {
   const [email, setEmail] = useState(undefined);
@@ -15,6 +15,7 @@ export default function SignUpScreen({ navigation }) {
   const [password, setPassword] = useState(undefined);
   const [disabled, setDisabled] = useState(true);
   const [passwordVisible, setPasswordVisible] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -24,21 +25,11 @@ export default function SignUpScreen({ navigation }) {
         <View style={styles.fieldsInput}>
           <TouchableOpacity style={styles.textContainer}>
             <Small fontSmall="Username"></Small>
-            <TextInput
-              style={styles.userInput}
-              theme={{ colors: { text: "white" } }}
-              value={username}
-              onChangeText={setUsername}
-            ></TextInput>
+            <TextInput style={styles.userInput} theme={{ colors: { text: 'white' } }} value={username} onChangeText={setUsername}></TextInput>
           </TouchableOpacity>
           <TouchableOpacity style={styles.textContainer}>
             <Small fontSmall="Email"></Small>
-            <TextInput
-              style={styles.userInput}
-              theme={{ colors: { text: "white" } }}
-              value={email}
-              onChangeText={setEmail}
-            ></TextInput>
+            <TextInput style={styles.userInput} theme={{ colors: { text: 'white' } }} value={email} onChangeText={setEmail}></TextInput>
           </TouchableOpacity>
           <TouchableOpacity style={styles.textContainer}>
             <Small fontSmall="Password"></Small>
@@ -46,11 +37,11 @@ export default function SignUpScreen({ navigation }) {
               onPress={() => setPasswordVisible(!passwordVisible)}
               onChangeText={setPassword}
               style={styles.userInput}
-              theme={{ colors: { text: "white" } }}
+              theme={{ colors: { text: 'white' } }}
               secureTextEntry={passwordVisible}
               right={
                 <TextInput.Icon
-                  name={passwordVisible ? "eye" : "eye-off"}
+                  name={passwordVisible ? 'eye' : 'eye-off'}
                   onPress={() => setPasswordVisible(!passwordVisible)}
                   onChangeText={setPassword}
                 />
@@ -61,13 +52,12 @@ export default function SignUpScreen({ navigation }) {
       </View>
       <View style={styles.buttonsbottom}>
         <TouchableOpacity
-          style={
-            !username || !email || !password ? styles.disabled : styles.normal
-          }
-          onPress={() => Register({ email, username, password, navigation })}
+          style={!username || !email || !password ? styles.disabled : styles.normal}
+          onPress={() => Register({ email, username, password, navigation }, setIsLoading(true)).then(() => setIsLoading(false))}
           disabled={!username || !email || !password}
         >
           <Buttons naming="Sign Up"></Buttons>
+          {isLoading === true && <ActivityIndicator style={styles.loading} color={'#fff'} />}
         </TouchableOpacity>
       </View>
     </View>
@@ -75,15 +65,15 @@ export default function SignUpScreen({ navigation }) {
 }
 const styles = StyleSheet.create({
   container: {
-    padding: "3%",
+    padding: '3%',
   },
 
   fieldsInput: {
-    top: "3%",
+    top: '3%',
   },
 
   buttonsbottom: {
-    top: "10%",
+    top: '10%',
   },
 
   disabled: {
@@ -94,14 +84,22 @@ const styles = StyleSheet.create({
   },
   userInput: {
     height: 55,
-    backgroundColor: "transparent",
-    borderColor: "#667080",
+    backgroundColor: 'transparent',
+    borderColor: '#667080',
     borderWidth: 1,
     borderRadius: 16,
     borderTopEndRadius: 16,
     borderTopStartRadius: 16,
     paddingHorizontal: 10,
-    width: "100%",
-    alignSelf: "center",
+    width: '100%',
+    alignSelf: 'center',
+  },
+  loading: {
+    position: 'absolute',
+    zIndex: 10000,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
   },
 });
