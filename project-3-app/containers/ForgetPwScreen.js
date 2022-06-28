ec90e89d17d493b257414f763aa00b1349183952
Gladys <98092633+gladysy@users.noreply.github.com>
2022-06-28 23:56:00 +0800

    forgotpwscreen frontend validation

--- project-3-app/containers/ForgetPwScreen.js
+++ project-3-app/containers/ForgetPwScreen.js
@@ -6,8 +6,13 @@ import {
   View,
   Button,
   SafeAreaView,
+  Pressable,
 } from "react-native";
 import { TextInput } from "react-native-paper";
+
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Validator from "email-validator";
 
 import Buttons from "../components/Buttons";
 import Register from "../api/Register";
@@ -18,6 +23,9 @@ import Underline from "../assets/Poppins_Underline";
 import Med from "../assets/Poppins_Medium";
 
 export default function ForgetPwScreen({ navigation, props }) {
+  const EmailSchema = Yup.object().shape({
+    email: Yup.string().email().required("Please enter your email"),
+  });
   return (
     <SafeAreaView>
       <SecondHeaderBar />
@@ -30,20 +38,50 @@ export default function ForgetPwScreen({ navigation, props }) {
 
           <Med fontMed={"We will send you an email."}></Med>
         </View>
-        <View style={{ paddingBottom: "15%", paddingTop: "5%" }}>
-          <Small fontSmall="Enter your registered email below"></Small>
-          <TouchableOpacity>
-            <TextInput
-              onChangeText={(text) => validate(text)}
-              style={styles.userInput}
-              keyboardType="email-address"
-              theme={{ colors: { text: "rgba(255, 255, 255, 0.6)" } }}
-            ></TextInput>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity>
-          <Buttons naming="Send Email"></Buttons>
-        </TouchableOpacity>
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+          validationSchema={EmailSchema}
+          validateOnMount={true}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, isValid }) => (
+            <>
+              <View style={{ paddingBottom: "15%", paddingTop: "5%" }}>
+                <Small fontSmall="Enter your registered email below"></Small>
+                <Pressable>
+                  <TextInput
+                    style={[
+                      styles.userInput,
+                      {
+                        borderColor:
+                          values.email.length < 1 ||
+                          Validator.validate(values.email)
+                            ? "rgba(255, 255, 255, 0.4)"
+                            : "rgba(244, 107, 107, 0.4)",
+                      },
+                    ]}
+                    keyboardType="email-address"
+                    textContentType="emailAddress"
+                    autoFocus={true}
+                    theme={{ colors: { text: "rgba(255, 255, 255, 0.6)" } }}
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    value={values.email}
+                  ></TextInput>
+                </Pressable>
+              </View>
+
+              <Pressable
+                onPress={handleSubmit}
+                style={styles.sendEmail(isValid)}
+              >
+                <Buttons naming="Send Email"></Buttons>
+              </Pressable>
+            </>
+          )}
+        </Formik>
       </View>
     </SafeAreaView>
   );
@@ -66,4 +104,8 @@ const styles = StyleSheet.create({
     width: "100%",
     alignSelf: "center",
   },
+
+  sendEmail: (isValid) => ({
+    opacity: isValid ? 1 : 0.4,
+  }),
 });
