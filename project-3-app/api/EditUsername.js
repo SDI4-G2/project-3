5987a2aabadc1fda0aba85c01308193804b889f6
Ewen <ewenlke@hotmail.com>
2022-07-04 02:20:21 +0800

    complete update username

---
+++ project-3-app/api/EditUsername.js
@@ -0,0 +1,26 @@
+export default async function EditUsername({ email, username, password, navigation }) {
+  return await fetch('https://sdi4-g2.herokuapp.com/editusername', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      username: username,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        alert('Username edited successfully.');
+        return false;
+      } else {
+        alert(res.message);
+        return true;
+      }
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
