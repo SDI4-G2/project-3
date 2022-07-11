4a64952759cd4fff97c6104e16b6c177580c9cf2
Gladys <98092633+gladysy@users.noreply.github.com>
2022-07-11 22:23:04 +0800

    live search using OnChange

--- project-3-app/containers/SearchScreen.js
+++ project-3-app/containers/SearchScreen.js
@@ -1,30 +1,38 @@
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Button, ActivityIndicator, Image, ImageBackground } from 'react-native';
-import { React, useState, useEffect } from 'react';
-import { TextInput } from 'react-native-paper';
-import { Card, Title } from 'react-native-paper';
-import { StackActions, TabRouter } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Button,
+  ActivityIndicator,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { React, useState, useEffect } from "react";
+import { TextInput } from "react-native-paper";
+import { Card, Title } from "react-native-paper";
+import { StackActions, TabRouter } from "@react-navigation/native";
 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import OctiIcons from 'react-native-vector-icons/Octicons';
-import HeaderBar from '../components/Headers';
-import Small from '../assets/Poppins_Small';
-import searchIcon from '../assets/searchIcon.png';
-import Med from '../assets/Poppins_Medium';
-import { CommonActions } from '@react-navigation/native';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import OctiIcons from "react-native-vector-icons/Octicons";
+import HeaderBar from "../components/Headers";
+import Small from "../assets/Poppins_Small";
+import searchIcon from "../assets/searchIcon.png";
+import Med from "../assets/Poppins_Medium";
 
-import { PulseIndicator } from 'react-native-indicators';
+import { PulseIndicator } from "react-native-indicators";
 
-import GetVideo from '../api/GetVideo';
-import GetArticle from '../api/GetArticle';
+import GetVideo from "../api/GetVideo";
+import GetArticle from "../api/GetArticle";
 
 export default function SearchScreen({ navigation, route }) {
   const [isLoading, setIsLoading] = useState(true);
   const [videos, setVideos] = useState([]);
   const [articles, setArticles] = useState([]);
-  const [search, setSearch] = useState([]);
+  const [search, setSearch] = useState("");
   const [vidResult, setVidResult] = useState([]);
   const [artResult, setArtResult] = useState([]);
-  const [showBody, setShowBody] = useState(false);
   const [showSearchImage, setShowSearchImage] = useState(true);
   const description = route.params;
 
@@ -34,7 +42,7 @@ export default function SearchScreen({ navigation, route }) {
     for (let i = 0; i < video.length; i++) {
       let vid = video[i];
       for (var key in vid) {
-        if (key !== 'Category' && vid.Category) {
+        if (key !== "Category" && vid.Category) {
           vid.Category[key] = vid[key];
         }
       }
@@ -43,7 +51,7 @@ export default function SearchScreen({ navigation, route }) {
     for (let i = 0; i < article.length; i++) {
       let art = article[i];
       for (var key in art) {
-        if (key !== 'Category' && art.Category) {
+        if (key !== "Category" && art.Category) {
           art.Category[key] = art[key];
         }
       }
@@ -53,14 +61,13 @@ export default function SearchScreen({ navigation, route }) {
     setArticles(article);
   }
 
-  function textSearch() {
+  function textSearch(e) {
     const vid = videos;
     const art = articles;
-    const keys = ['description', 'text', 'title'];
+    const keys = ["description", "text", "title"];
     const values = [search];
-    const regex = new RegExp(values, 'i');
+    const regex = new RegExp(values, "i");
 
-    setShowBody(true);
     setShowSearchImage(false);
 
     const filteredVid = vid.filter((e) => {
@@ -76,6 +83,7 @@ export default function SearchScreen({ navigation, route }) {
       });
     });
     setArtResult(filteredArt);
+    console.log(search);
   }
 
   function passSearch() {
@@ -89,8 +97,11 @@ export default function SearchScreen({ navigation, route }) {
   }, []);
 
   useEffect(() => {
-    textSearch();
-  }, [search, videos, articles]);
+    if (search.length === 0) {
+      setShowSearchImage(true);
+    } else setShowSearchImage(false);
+    console.log(search);
+  }, [search]);
 
   useEffect(() => {
     passSearch();
@@ -99,7 +110,7 @@ export default function SearchScreen({ navigation, route }) {
   return (
     <View>
       <HeaderBar />
-      <View style={{ width: '80%', alignSelf: 'center', top: '0%' }}>
+      <View style={{ width: "80%", alignSelf: "center", top: "0%" }}>
         <View>
           <TouchableOpacity style={styles.textContainer}>
             <TextInput
@@ -110,14 +121,29 @@ export default function SearchScreen({ navigation, route }) {
               style={[styles.userInput]}
               returnKeyType="search"
               onSubmitEditing={textSearch}
-              defaultValue={search}
+              onChange={() => {
+                textSearch();
+              }}
+              // onChange={() => {
+              //   textSearch();
+              // }}
+              // defaultValue={search}
               theme={{
                 colors: {
-                  text: 'rgba(255, 255, 255, 0.6)',
+                  text: "rgba(255, 255, 255, 0.6)",
                 },
               }}
               right={
-                <TextInput.Icon name={() => <OctiIcons name={'search'} size={20} color={'rgba(255,255,255,0.5)'} onPress={() => textSearch()} />} />
+                <TextInput.Icon
+                  name={() => (
+                    <OctiIcons
+                      name={"search"}
+                      size={20}
+                      color={"rgba(255,255,255,0.5)"}
+                      onPress={() => textSearch()}
+                    />
+                  )}
+                />
               }
             ></TextInput>
           </TouchableOpacity>
@@ -134,37 +160,39 @@ export default function SearchScreen({ navigation, route }) {
                 style={{
                   height: 200,
                   width: 210,
-                  alignSelf: 'center',
-                  top: '35%',
+                  alignSelf: "center",
+                  top: "35%",
                 }}
               />
-              <View style={{ top: '40%', alignSelf: 'center' }}>
-                <Small fontSmall={'Type anything to find what you are looking for.'} />
+              <View style={{ top: "40%", alignSelf: "center" }}>
+                <Small
+                  fontSmall={"Type anything to find what you are looking for."}
+                />
               </View>
             </>
-          ) : null}
-
-          {showBody ? (
+          ) : (
             <>
               <View style={{ marginBottom: 20 }}>
-                <View style={{ paddingTop: '5%' }}>
-                  <Small fontSmall={'Videos'}></Small>
+                <View style={{ paddingTop: "5%" }}>
+                  <Small fontSmall={"Videos"}></Small>
                 </View>
                 <ScrollView
                   horizontal={true}
                   contentContainerStyle={{
-                    justifyContent: 'center',
-                    flexDirection: 'row',
+                    justifyContent: "center",
+                    flexDirection: "row",
                   }}
                 >
-                  {isLoading === true && <PulseIndicator color={'rgba(255,255,255,0.5)'} />}
+                  {isLoading === true && (
+                    <PulseIndicator color={"rgba(255,255,255,0.5)"} />
+                  )}
                   {vidResult.map((item) => {
                     return (
                       <Card
                         style={styles.cardDashboard}
                         key={item.videoid}
                         onPress={() => {
-                          navigation.navigate('VideoScreen', {
+                          navigation.navigate("VideoScreen", {
                             videoid: item.videoid,
                           });
                         }}
@@ -176,7 +204,7 @@ export default function SearchScreen({ navigation, route }) {
                           imageStyle={{
                             borderRadius: 15,
                             opacity: 0.5,
-                            backgroundColor: '#000',
+                            backgroundColor: "#000",
                           }}
                           onLoadEnd={() => setIsLoading(false)}
                         >
@@ -196,21 +224,23 @@ export default function SearchScreen({ navigation, route }) {
               </View>
 
               <View style={{ marginBottom: 20 }}>
-                <Small fontSmall={'Articles'}></Small>
+                <Small fontSmall={"Articles"}></Small>
                 <ScrollView
                   horizontal={true}
                   contentContainerStyle={{
-                    justifyContent: 'center',
-                    flexDirection: 'row',
+                    justifyContent: "center",
+                    flexDirection: "row",
                   }}
                 >
-                  {isLoading === true && <PulseIndicator color={'rgba(255,255,255,0.5)'} />}
+                  {isLoading === true && (
+                    <PulseIndicator color={"rgba(255,255,255,0.5)"} />
+                  )}
                   {artResult.map((item, index) => (
                     <Card
                       style={styles.cardDashboard}
                       key={item.articleid}
                       onPress={() => {
-                        navigation.navigate('ArticleScreen', {
+                        navigation.navigate("ArticleScreen", {
                           articleid: item.articleid,
                         });
                       }}
@@ -222,12 +252,14 @@ export default function SearchScreen({ navigation, route }) {
                         imageStyle={{
                           borderRadius: 15,
                           opacity: 0.5,
-                          backgroundColor: '#000',
+                          backgroundColor: "#000",
                         }}
                         onLoadEnd={() => setIsLoading(false)}
                       >
                         <Card.Content>
-                          <Title style={styles.cardTitle}>{item.description}</Title>
+                          <Title style={styles.cardTitle}>
+                            {item.description}
+                          </Title>
                         </Card.Content>
                       </ImageBackground>
                     </Card>
@@ -235,7 +267,7 @@ export default function SearchScreen({ navigation, route }) {
                 </ScrollView>
               </View>
             </>
-          ) : null}
+          )}
         </View>
       </View>
     </View>
@@ -244,22 +276,22 @@ export default function SearchScreen({ navigation, route }) {
 
 const styles = StyleSheet.create({
   textContainer: {
-    overflow: 'hidden',
+    overflow: "hidden",
     height: 40,
     borderRadius: 16,
     borderWidth: 1,
-    borderColor: 'rgba(255, 255, 255, 0.3)',
+    borderColor: "rgba(255, 255, 255, 0.3)",
   },
 
   userInput: {
     height: 40,
-    backgroundColor: 'rgba(255, 255,255, 0.05)',
+    backgroundColor: "rgba(255, 255,255, 0.05)",
     borderTopEndRadius: 16,
     borderTopStartRadius: 16,
     paddingHorizontal: 10,
-    width: '100%',
-    alignSelf: 'center',
-    color: 'rgba(255,255,255,0.7)',
+    width: "100%",
+    alignSelf: "center",
+    color: "rgba(255,255,255,0.7)",
   },
   cardDashboard: {
     width: 120,
@@ -267,30 +299,30 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     marginRight: 15,
-    borderColor: 'rgba(102, 112, 128, 0.3)',
+    borderColor: "rgba(102, 112, 128, 0.3)",
     borderRadius: 15,
-    backgroundColor: '#d2d5db',
+    backgroundColor: "#d2d5db",
     borderWidth: 1,
     fontWeight: 700,
   },
   cardTitle: {
-    fontStyle: 'normal',
-    fontWeight: '200',
+    fontStyle: "normal",
+    fontWeight: "200",
     fontSize: 12,
     lineHeight: 20,
-    display: 'flex',
-    alignSelf: 'center',
-    textAlign: 'center',
-    color: 'rgba(255, 255, 255, 0.7)',
+    display: "flex",
+    alignSelf: "center",
+    textAlign: "center",
+    color: "rgba(255, 255, 255, 0.7)",
   },
   cardText: {
-    fontStyle: 'italic',
-    fontWeight: '400',
+    fontStyle: "italic",
+    fontWeight: "400",
     fontSize: 12,
     lineHeight: 30,
-    display: 'flex',
-    alignSelf: 'center',
-    color: 'rgba(255, 255, 255, 0.7)',
+    display: "flex",
+    alignSelf: "center",
+    color: "rgba(255, 255, 255, 0.7)",
   },
   cardImage: {
     width: 120,
