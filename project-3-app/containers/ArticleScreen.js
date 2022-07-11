0b8b47bceb92a5aa6d44c7aa2c4ebdc018f2337c
Ewen <ewenlke@hotmail.com>
2022-07-11 23:30:49 +0800

    updated article page

--- project-3-app/containers/ArticleScreen.js
+++ project-3-app/containers/ArticleScreen.js
@@ -1,23 +1,109 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  ScrollView,
-  Image,
-  TouchableOpacity
-} from "react-native";
-import HeaderBar from "../components/Headers";
-import { Card } from "react-native-paper";
-import MedCenter from "../assets/Poppins_CenterTitle";
-import bitcoinPic from "../assets/bitcoin3.5.png";
-import PreviousButton from "../components/PreviousButton";
-import NextButton from "../components/NextButton";
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import HeaderBar from '../components/Headers';
+import { Card } from 'react-native-paper';
+import MedCenter from '../assets/Poppins_CenterTitle';
+import bitcoinPic from '../assets/bitcoin3.5.png';
+import PreviousButton from '../components/PreviousButton';
+import NextButton from '../components/NextButton';
 import { WebView } from 'react-native-webview';
+import { useIsFocused } from '@react-navigation/native';
 
-const PdfReader = ({ url: uri }) => <WebView style={{ flex: 1 }} source={{ uri }} />
+import GetArticle from '../api/GetArticle';
 
-export default function ArticleScreen({ wording }) {
+const PdfReader = ({ url: uri }) => <WebView style={{ flex: 1 }} source={{ uri }} />;
+
+export default function ArticleScreen({ wording, route }) {
+  const [articles, setArticles] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [bg, setBg] = useState(null);
+  const [list, setList] = useState(undefined);
+  const [currentArticle, setCurrentArticle] = useState(undefined);
+  const [endOfFrontLine, setEndOfFrontLine] = useState(true);
+  const [endOfEndLine, setEndOfEndLine] = useState(true);
+  const { articleid, url } = route.params;
+  const isFocused = useIsFocused();
+
+  async function get() {
+    const articles = await GetArticle();
+    setArticles(articles);
+    let newList = articles.filter((art) => {
+      return art.articleid === articleid;
+    });
+    setList(newList[0].url);
+    setTitle(newList[0].title);
+    setBg(newList[0].thumbnails);
+    setCurrentArticle(newList[0].articleid);
+  }
+
+  function endOfLineCheck() {
+    if (articles) {
+      let last = articles.length - 1;
+      if (currentArticle === articles[0].articleid && currentArticle === articles[last].articleid) {
+        setEndOfFrontLine(true);
+        setEndOfEndLine(true);
+      } else if (currentArticle === articles[0].articleid) {
+        setEndOfFrontLine(true);
+      } else if (currentArticle === articles[last].articleid) {
+        setEndOfEndLine(true);
+      } else {
+        setEndOfFrontLine(false);
+        setEndOfEndLine(false);
+      }
+    }
+  }
+
+  function prevArticle() {
+    try {
+      for (let i = 0; i < articles.length; i++) {
+        if (articles[i].articleid === currentArticle) {
+          setList(articles[i - 1].url);
+          setCurrentArticle(articles[i - 1].articleid);
+          setTitle(articles[i - 1].title);
+          setBg(articles[i - 1].thumbnails);
+          endOfLineCheck();
+          // console.log(title);
+        }
+      }
+    } catch (err) {
+      alert('Unable to previous' + err);
+    }
+  }
+
+  function nextArticle() {
+    try {
+      for (let i = 0; i < articles.length; i++) {
+        if (articles[i].articleid === currentArticle) {
+          setList(articles[i + 1].url);
+          setCurrentArticle(articles[i + 1].articleid);
+          setTitle(articles[i + 1].title);
+          setBg(articles[i + 1].thumbnails);
+          endOfLineCheck();
+        }
+      }
+    } catch (err) {
+      console.log('Unable to next' + err);
+    }
+  }
+
+  function passSearch() {
+    if (articleid) {
+      get();
+    }
+  }
+
+  useEffect(() => {
+    get();
+  }, []);
+
+  useEffect(() => {
+    endOfLineCheck();
+  });
+
+  useEffect(() => {
+    passSearch();
+  }, [articleid]);
+
   return (
     <View>
       <HeaderBar />
@@ -29,54 +115,33 @@ export default function ArticleScreen({ wording }) {
               <View
                 style={{
                   flex: 1,
-                  position: "absolute",
-                  alignSelf: "center",
-                  height: "100%",
-                  justifyContent: "center",
-                  maxWidth: "90%",
+                  position: 'absolute',
+                  alignSelf: 'center',
+                  height: '100%',
+                  justifyContent: 'center',
+                  maxWidth: '90%',
                   // borderWidth: 3,
                   opacity: 0.7,
                 }}
               >
-                <MedCenter
-                  fontMedCenter={"Bitcoin Thief Resort to Stealing Pencils"}
-                />
+                <MedCenter fontMedCenter={title} />
               </View>
             </View>
 
             <Card.Content>
               <ScrollView style={styles.content}>
-                <View style={{height: 750, paddingVertical:15}}>
-                <PdfReader url="https://drive.google.com/file/d/1pULV4WNtSLZYuBEi2hetq8bqFen9MtAR/view" />
+                <View style={{ height: 750, paddingVertical: 15 }}>
+                  <PdfReader url={list} />
                 </View>
-                {/* <Text style={styles.text}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a
-                  massa enim. Sed arcu erat, facilisis a libero at, ultricies
-                  sagittis orci. Nam consectetur cursus tellus. Proin commodo,
-                  velit eu dignissim mollis Lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit. In a massa enim. Sed arcu erat,
-                  facilisis a libero at, ultricies sagittis orci. Nam
-                  consectetur cursus tellus. Proin commodo, velit eu dignissim
-                  mollis, diam justo feugiat mauris, sed imperdiet tortor nibh
-                  ornare ex. Aenean vehicula dui et pellentesque mollis. Aenean
-                  ac elementum lectus. Suspendisse accumsan, ex sit amet ornare
-                  rhoncus, lorem nisl placerat quam, vel lacinia dui orci eu
-                  ante. Vivamus vestibulum urna nisi, quis dignissim orci
-                  suscipit rutrum. Praesent sodales nulla vitae tortor
-                  vestibulum consequat eu ut felis. Ut tincidunt dolor quis elit
-                  molestie tincidunt.
-                </Text> */}
               </ScrollView>
             </Card.Content>
           </Card>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <TouchableOpacity>
-              <PreviousButton wording={"Article"} />
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <TouchableOpacity onPress={() => prevArticle()} disabled={endOfFrontLine === true} style={endOfFrontLine ? styles.disabled : null}>
+              <PreviousButton wording={'Article'} />
             </TouchableOpacity>
-            <TouchableOpacity>
-              <NextButton wording={"Article"} />
+            <TouchableOpacity onPress={() => nextArticle()} disabled={endOfEndLine === true} style={endOfEndLine ? styles.disabled : null}>
+              <NextButton wording={'Article'} />
             </TouchableOpacity>
           </View>
         </View>
@@ -88,30 +153,30 @@ export default function ArticleScreen({ wording }) {
 const styles = StyleSheet.create({
   card: {
     borderRadius: 30,
-    width: "100%",
-    alignSelf: "center",
-    overflow: "hidden",
-    paddingBottom: "30%",
+    width: '100%',
+    alignSelf: 'center',
+    overflow: 'hidden',
+    paddingBottom: '30%',
   },
   background: {
-    backgroundColor: "rgba(89,60,21, 0.3)",
+    backgroundColor: 'rgba(89,60,21, 0.3)',
     borderRadius: 25,
-    borderColor: "rgba(89,60,21, 0.5)",
-    width: "80%",
-    alignSelf: "center",
+    borderColor: 'rgba(89,60,21, 0.5)',
+    width: '80%',
+    alignSelf: 'center',
   },
 
   content: {
     borderRadius: 30,
-    height: "53%",
-    width: "100%",
-    alignSelf: "center",
+    height: '53%',
+    width: '100%',
+    alignSelf: 'center',
   },
 
   text: {
-    textAlign: "left",
-    fontFamily: "Poppins_300Light",
-    color: "white",
+    textAlign: 'left',
+    fontFamily: 'Poppins_300Light',
+    color: 'white',
     opacity: 0.7,
     // top: "3%",
     fontSize: 13,
@@ -119,16 +184,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     lineHeight: 30,
     // width: "90%",
-    alignSelf: "center",
-    height: "100%",
+    alignSelf: 'center',
+    height: '100%',
   },
 
   image: {
     borderRadius: 30,
-    alignSelf: "center",
-    overflow: "hidden",
+    alignSelf: 'center',
+    overflow: 'hidden',
     opacity: 1,
-    width: "100%",
+    width: '100%',
     height: 200,
+  },
+  disabled: {
+    opacity: 0.3,
   },
 });
